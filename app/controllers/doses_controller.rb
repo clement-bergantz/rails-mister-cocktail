2139cf142a3bfbcf9c9be3ad048eb9c8af25aef3
clement bergantz <bergantz.clement@gmail.com>
2017-08-10 18:40:04 +0200

    add 3 models : Cocktail, Dose, Ingredient + 3 controllers + routing + vew + actions (index, show, new, create)

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:create, :new] 
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+end
